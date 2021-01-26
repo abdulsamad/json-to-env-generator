d5c9a799e364712be5818ab72b7365ca553ab5ea
Abdul Samad <Samk8778@yahoo.com>
2021-01-26 21:40:49 +0530

    :speech_balloon: Changed dummy config

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -2,7 +2,7 @@ export const dummyConfigJSON = {
 	apiKey: '1afd8097-75fa-5a72-8403-66ee57014e3a',
 	authDomain: 'http://wuvle.eg/wisolhel',
 	projectId: 'tXHnZiUkPNBUmz1z0',
-	storageBucket: 'JysWqomum27JPwciP46',
+	storageBucket: 'http://zagu.es/zahejlop',
 	messagingSenderId: '9397484',
 	appId: '7311:4c91:411f:5ce7:6f76:f67e:37d2:a997',
 };
