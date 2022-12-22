3b9f8d92ae99466661efd7cfacec80ed9322d2c2
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-22 15:15:47 +0530

    Added footer and navbar

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,11 @@
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p class="has-text-white-bis mb-1">
+			Developed with <a href="https://svelte.dev" class="has-text-primary">Svelte</a>
+		</p>
+		<p class="has-text-white-bis">
+			Made with <span class="has-text-danger">&#10084;</span> by
+			<a class="has-text-primary" href="https://abdulsamad.dev">AbdulSamad</a>
+		</p>
+	</div>
+</footer>
