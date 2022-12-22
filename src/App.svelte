cbc39ef6c12146804cda02fa9c79ef5fef12cc1f
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-22 16:08:13 +0530

    :construction: Updated main app component

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import { showResult } from './stores';
+
+	import Navbar from './lib/Navbar.svelte';
+	import TextArea from './lib/TextArea.svelte';
+	import Footer from './lib/Footer.svelte';
+	import Result from './lib/Result.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<Navbar />
+	<TextArea />
+	{#if $showResult}
+		<Result />
+	{/if}
+	<Footer />
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+<style lang="scss">
+	main {
+		min-height: 100vh;
+		width: 100vw;
+	}
 </style>
