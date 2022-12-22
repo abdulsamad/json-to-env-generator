2773d77d61d05f0598137a5b6d2702e02ccce3b3
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-12-22 16:23:26 +0530

    :lipstick: Updated navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,5 @@
 <nav class="navbar is-primary" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a class="navbar-item is-size-4 has-text-weight-semibold is-italic" href="/"
-			>JSON to Env Generator
-		</a>
+		<a class="navbar-item is-size-4 has-text-weight-semibold" href="/">JSON to Env Generator </a>
 	</div>
 </nav>
